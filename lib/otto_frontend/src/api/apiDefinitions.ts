cbdae5188ac02f79d3892626e462d9731af2bce0
Jake <jakechen825@gmail.com>
2025-02-10 22:27:05 -0500

    feat: add axios instance and add api types

--- lib/otto_frontend/src/api/apiDefinitions.ts
+++ lib/otto_frontend/src/api/apiDefinitions.ts
@@ -1,1 +1,40 @@
+import { LoginBody, LoginResponse, LogoutResponse } from "@/types/api";
+
 export const baseAPI = import.meta.env.VITE_BASE_API_URL;
+
+enum ApiMethods {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+  PATCH = 'PATCH',
+}
+
+export enum EndpointKeys {
+  USER_LOGIN = 'USER_LOGIN',
+  USER_LOGOUT = 'USER_LOGOUT',
+}
+
+export interface EndpointType<Params, Body, Response> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  url: (arg: any) => string;
+  queryParams?: Params;
+  body?: Body;
+  response: Response;
+}
+
+export interface ApiEndpointsType {
+  [EndpointKeys.USER_LOGIN]: EndpointType<null, LoginBody, LoginResponse>;
+  [EndpointKeys.USER_LOGOUT]: EndpointType<null, null, LogoutResponse>;
+}
+
+export const Endpoints = {
+  [EndpointKeys.USER_LOGIN]: {
+    url: () => `/login/`,
+    method: ApiMethods.POST,
+  },
+  [EndpointKeys.USER_LOGOUT]: {
+    url: () => `/logout/`,
+    method: ApiMethods.POST,
+  },
+}
---
+++ lib/otto_frontend/src/types/api/index.ts
@@ -0,0 +1,8 @@
+import { LoginBody, LoginResponse } from "./AuthResponse/Login";
+import { LogoutResponse } from "./AuthResponse/Logout";
+
+export type {
+  LoginBody,
+  LoginResponse,
+  LogoutResponse
+}
